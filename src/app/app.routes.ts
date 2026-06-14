2d781996efdc14275583b66fd8ab453350dce0f0
issa23i <isabelpastorlopezcv@gmail.com>
2026-06-14 18:11:26 +0200

    feat: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { QuienesSomos } from './features/quienes-somos/quienes-somos';
+import { Federaciones } from './features/federaciones/federaciones';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Red Vecinal Andaluza – Inicio',
+  },
+  {
+    path: 'quienes-somos',
+    component: QuienesSomos,
+    title: 'Quiénes Somos – RedVS',
+  },
+  {
+    path: 'federaciones',
+    component: Federaciones,
+    title: 'Federaciones – RedVS',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
